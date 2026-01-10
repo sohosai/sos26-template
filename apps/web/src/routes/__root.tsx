7e7559d1c917a76b788c7a9f0c88081470e19ad1
pocopota <pocopota.program@gmail.com>
2026-01-11 02:44:24 +0900

    エラーページの整備 & devtoolsの表示整備

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -3,6 +3,8 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
 	component: RootComponent,
+	errorComponent: ErrorComponent,
+	notFoundComponent: NotFoundComponent,
 });
 
 function RootComponent() {
@@ -10,7 +12,23 @@ function RootComponent() {
 		<>
 			<HeadContent />
 			<Outlet />
-			<TanStackRouterDevtools />
+			{import.meta.env.DEV && <TanStackRouterDevtools />}
 		</>
+	);
+}
+
+function NotFoundComponent() {
+	return <div>404 Not Found.</div>;
+}
+
+function ErrorComponent({ error }: { error: unknown }) {
+	const message =
+		error instanceof Error ? error.message : String(error ?? "Unknown error");
+
+	return (
+		<div>
+			<div>Error Occered</div>
+			<div>{message}</div>
+		</div>
 	);
 }
